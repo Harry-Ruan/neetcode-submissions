4c77d02c441753b8d719e09255dfed8562788094
Harry-Ruan <3555748553@qq.com>
2026-09-14 18:48:09 +0800

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> res;
+    int depth = 0;
+    vector<int> rightSideView(TreeNode* root) {
+        dfs(root, 1);
+        return res;
+    }
+
+    void dfs(TreeNode* cur, int level){
+        if (!cur) return;
+        if (level > depth){
+            res.push_back(cur->val);
+            depth++;
+        }
+        dfs(cur->right, level + 1);
+        dfs(cur->left, level + 1);
+    }
+};
