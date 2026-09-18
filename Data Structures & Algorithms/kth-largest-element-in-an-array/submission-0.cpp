e642e5218e7209914b91ecbda86d0e948d84a77f
Harry-Ruan <3555748553@qq.com>
2026-09-18 11:47:22 +0800

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minhp;
+        for(int i : nums){
+            minhp.push(i);
+            if (minhp.size() > k) minhp.pop();
+        }
+        return minhp.top();
+    }
+};
